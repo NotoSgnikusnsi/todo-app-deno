46d71fb5561e7e4edb51fb56032dfbc769107efa
Noto <notosgnikusnsi@gmail.com>
2023-08-28 19:35:37 +0900

    fix: デリートボタンを変更した

---
+++ public/script.js
@@ -0,0 +1,73 @@
+window.onload = async () => {
+  try {
+    const response = await fetch('/get-tasks');
+    const json = await response?.json();
+    const taskList = document.getElementById("tasks");
+    if (json && json[0] !== undefined) {
+      for (let i = 0; i < json.length ; i++) {
+        // box
+        const boxDiv = document.createElement("div");
+        boxDiv.classList.add("box");
+
+        // タスクを表示させる場所
+        const p = document.createElement("p")
+        p.id = json[i]["_id"]
+        const taskTitle = json[i]['task'];
+        const taskComplete = json[i]["complete"] ? '完了済' : '未完了'
+        const content = taskTitle + ":" + taskComplete;
+        p.innerHTML = content;
+
+        // 削除ボタン
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete");
+        deleteButton.onclick = handleDeleteButtonClick;
+
+        p.prepend(deleteButton);
+        boxDiv.appendChild(p)
+        taskList.appendChild(boxDiv);
+
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+document.getElementById('todo-button').onclick = async (e) => {
+  e.preventDefault();
+  const task = document.getElementById('todo-input').value;
+  try {
+    // display loading icon
+    document.getElementById('todo-button').classList.add('is-loading');
+
+    // POST task
+    await fetch('/add-task', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ task: task }),
+    });
+
+    // hidden loading icon
+    document.getElementById('todo-button').classList.remove('is-loading');
+    // reload
+    window.location.reload();
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+};
+
+const handleDeleteButtonClick = async(e) => {
+  e.preventDefault();
+  if (confirm("削除します") === false) {
+    return;
+  }
+  const id = e.target.parentNode.id;
+  try {
+    await fetch("/delete-task?id=" + id)
+    location.reload();
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
